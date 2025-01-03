8b4b164891d2ec295ca0f23e9ae5dfc1c19b3da6
John Diego de Oliveira Silva <johnoliveirasilva9@gmail.com>
2025-01-03 15:17:33 -0300

    passing props

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,13 @@
 
-import { Card } from './styles'
+import { Card, Title } from './styles'
 
 export default () => {
   return (
     < Card>
-    ..
+      <Title $color="red" $background='black'>
+        Hello World
+      </Title>
+      <p>My first styled component</p>
     </Card>
   )
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -6,4 +6,11 @@ export const Card = styled.div`
   height: 200px;
   padding: 20px;
   color: green;
+`;
+
+export const Title = styled.span<{ $color: string, $background?: string }>`
+  font-size: 19px;
+  font-weight: 800;
+  color: ${props => props.$color};
+  background-color: ${props => props.$background};
 `
